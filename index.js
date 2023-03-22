08b8c2baaaa5ed7c6e4577fbd5343e6ff10ee564
Bethany Albright <balbright0186@gmail.com>
2023-03-22 03:08:25 -0400

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names =["Guadalupe", "Ollie", "Aki"];
+const messages = [];
+const event = ["surprise"]
+function writeCards(names, eventName){
+    for (let i = 0; i < names.length; i ++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);}
+        return messages;
+    }
+    
+    
+
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number);
+        number--; 
+    }
+}
